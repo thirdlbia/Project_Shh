4b87a71155bb2999c81a969c62db59ab2bfdcb6e
thirdlbia <thirdlbia@gmail.com>
2018-10-09 03:27:06 +0900

    기존 프로젝트 삭제

--- Source/Project_Shh/Project_Shh.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "Project_Shh.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Shh, "Project_Shh" );
- 
